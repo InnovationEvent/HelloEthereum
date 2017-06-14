bba4bac1f8e0691f574683c69c1b9518533f436a
rajrin <rajeevsakhuja@40HW015529.local>
2017-06-13 22:22:14 -0400

    added

---
+++ contracts/VotingSkeleton.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.4;
+
+/**
+ * SKELETON Code for voting
+ * 
+ * Contract gets initialized with a string - a question for which answer is Yes & NO
+ * Anyone can vote - either yes or no
+ * Everyone can vote only once
+ * Voting ends after fixed time
+ * 
+ * Step 2 Set a time limit to voting
+ **/
+contract Voting {
+
+
+  // Constructor
+  function  Voting(string quest){
+   
+  }
+
+  // Vote
+  function  vote(bool yesNo){
+
+  }
+
+  // Result
+  function  result() returns(string quest, uint yes, uint no){
+
+  }
+}
